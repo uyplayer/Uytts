b321154713b1905746509b3c72f0d373d54fca79
UYPLAYER <uyplayer@UYPLAYER.local>
2021-03-03 12:23:31 +0800

    okk

---
+++ src/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 2021/3/3 12:23 چۈشتىن كېيىن
+@Author  : uyplayer
+@Site    : uyplayer.pw
+@File    : test.py
+@contact : uyplayer@qq.com
+@Software: PyCharm
+"""
